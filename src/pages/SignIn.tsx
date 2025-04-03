c3c52f0598d992da72aa4fa6d6a90426cc80710c
gazimba <gabrielb3r@gmail.com>
2025-04-03 16:57:33 -0300

    trocando useState para useActionState

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,22 +1,19 @@
-import { FormEvent, useState } from "react";
+import { useActionState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function SignIn() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
+    const [state, formAction] = useActionState()
 
-    function onSubmit(e: FormEvent) {
-        e.preventDefault();
-        alert("enviado");
+    function onAction(formData: FormData) {
+      
     }
 
     return (
         <>
-            <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
-                <Input required legend="E-mail" type="email" placeholder="seuemail@exemplo.com" onChange={(e) => setEmail(e.target.value)} />
-                <Input required legend="Senha" type="password" placeholder="123456" onChange={(e) => setPassword(e.target.value)} />
+            <form action={onAction} className="w-full flex flex-col gap-4">
+                <Input name="email" required legend="E-mail" type="email" placeholder="seuemail@exemplo.com" />
+                <Input name="password" required legend="Senha" type="password" placeholder="123456" />
                 <Button type="submit" isLoading={isLoading}>Entrar</Button>
                 <a href="/signup" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">Criar conta</a>
             </form>
