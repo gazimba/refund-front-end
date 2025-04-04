7fe6b8673ec0025505b6c3a86e4c6bd7d83ef0bc
gazimba <gabrielb3r@gmail.com>
2025-04-04 14:44:57 -0300

    nome de usuario ao lado do botao de sair

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,13 @@ import logoutSvg from "../assets/logout.svg"
 import { useAuth } from "../hooks/useAuth"
 
 export function Header() {
-    const { remove } = useAuth()
+    const auth = useAuth()
     return (
         <header className="w-full flex justify-between">
             <img src={logoSvg} alt="logo" className="my-8" />
             <div className="flex items-center gap-3">
-                <span className="text-sm font-semibold text-gray-200">Olá, Usuário</span>
-                <img src={logoutSvg} alt="Icone de sair" className="cursor-pointer my-8 hover:opacity-75 transition ease-linear" onClick={() => remove()} />
+                <span className="text-sm font-semibold text-gray-200">Olá, {auth.session?.user.name} </span>
+                <img src={logoutSvg} alt="Icone de sair" className="cursor-pointer my-8 hover:opacity-75 transition ease-linear" onClick={() => auth.remove()} />
             </div>
         </header>
     )
